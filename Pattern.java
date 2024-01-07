8f5802a3e405e538e3d74a8aaaff3456f2783760
pooaraz <pooaraz@localhost.localdomain>
2024-01-07 13:43:22 +0530

    pattern

--- Pattern.java
+++ Pattern.java
@@ -92,6 +92,13 @@ public class Pattern {
             System.out.println();
           
         }
+
+        //  ***********
+        //   *********
+        //    *******
+        //     *****
+        //      ***
+        //       *
         for(int i=0;i<n;i++){
                 for(int j=0;j<=i;j++)
                 System.out.print(" ");
@@ -101,6 +108,61 @@ public class Pattern {
                 System.out.print("*");
             System.out.println();
         }
+             
+        // 1
+        // 22
+        // 333
+        // 4444
+        // 55555
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }
+        // 1
+        // 12
+        // 123
+        // 1234
+        // 12345
+         for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+        // 12345
+        // 1234
+        // 123
+        // 12
+        // 1
+          for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+
+
+        // 1
+        // 01
+        // 101
+        // 0101
+        // 10101
+        // 010101
+        for(int i=1;i<=n;i++){
+            int inc=0;
+            if(i%2!=0) System.out.print(++inc);
+            else System.out.print(inc);
+            for(int j=1;j<i;j++){
+                inc=1-inc;
+                System.out.print(inc);
+                }
+                System.out.println();
+            }
+        
+
+
 
     }
 }
